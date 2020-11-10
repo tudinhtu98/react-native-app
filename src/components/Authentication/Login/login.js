c75c302cd40b6ca6fb7bfebc69f07b490c24b136
Tu Dinh Tu <tudinhtu98@gmail.com>
2020-11-10 17:00:23 +0700

    Add login screen

---
+++ src/components/Authentication/Login/login.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+
+const Login = (props) => {
+  return (
+    <View style={styles.view}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Username (or Email)"
+        autoCompleteType="username"
+      ></TextInput>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Password"
+        autoCompleteType="password"
+        textContentType="password"
+        secureTextEntry={true}
+      ></TextInput>
+      <TouchableOpacity style={[styles.button, styles.buttonLogin]}>
+        <Text>SIGN IN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.textBlue}>FORGOT PASSWORD?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.textBlue}>REGISTER FREE</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  view: {
+    margin: 30,
+  },
+  textInput: {
+    height: 55,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+  },
+  button: {
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    borderRadius: 5,
+  },
+  buttonLogin: {
+    backgroundColor: "gray",
+  },
+  textBlue: {
+    color: "blue",
+  },
+});
