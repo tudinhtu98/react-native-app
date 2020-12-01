c56b7529b4ed039832a01a87ed731f3f2cab28ab
Tu Dinh Tu <tudinhtu98@gmail.com>
2020-12-01 20:18:44 +0700

    Add Handle Button Bookmark, Share in Course detail

--- src/components/CourseDetail/SectionIntro/section-intro.js
+++ src/components/CourseDetail/SectionIntro/section-intro.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   StyleSheet,
@@ -6,10 +6,22 @@ import {
   Alert,
   TouchableOpacity,
   ScrollView,
+  Share,
 } from "react-native";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 const SectionIntro = (props) => {
+  const [bookmarkIconName, setBookmarkIconName] = useState("bookmark-outline");
+  const handleBookmark = () => {
+    if (bookmarkIconName == "bookmark-outline") {
+      setBookmarkIconName("bookmark");
+    } else {
+      setBookmarkIconName("bookmark-outline");
+    }
+  };
+  const handleShareCourse = () => {
+    Share.share({ message: "share course" });
+  };
   return (
     <View style={styles.view}>
       <Text style={styles.title}>{props.courseInfo.title}</Text>
@@ -29,17 +41,11 @@ const SectionIntro = (props) => {
         style={[styles.darkText, { margin: 5 }]}
       >{`${props.courseInfo.level} . ${props.courseInfo.released} . ${props.courseInfo.duration}`}</Text>
       <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
-        <TouchableOpacity
-          style={styles.iconCenter}
-          onPress={() => Alert.alert("onPress Bookmark")}
-        >
-          <Icon name="bookmark-outline" size={25} color="gray" />
+        <TouchableOpacity style={styles.iconCenter} onPress={handleBookmark}>
+          <Icon name={bookmarkIconName} size={25} color="gray" />
           <Text>Bookmark</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.iconCenter}
-          onPress={() => Alert.alert("onPress Share")}
-        >
+        <TouchableOpacity style={styles.iconCenter} onPress={handleShareCourse}>
           <Icon name="share" size={25} color="gray" />
           <Text>Share</Text>
         </TouchableOpacity>
